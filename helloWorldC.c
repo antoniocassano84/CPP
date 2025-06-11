8c2c6bb5f514ed6cfbc4bfd7aada3999fa89a28e
antonio <ac.pollazzo@gmail.com>
2025-06-11 20:04:47 +0100

    second

--- helloWorldC.c
+++ helloWorldC.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+int fibonacci(int n) {
+    int prev = 0, curr = 1;
+    int i;
+
+    printf("%d\n%d\n", prev, curr);
+
+    for (i = 2; i <= n; i++) {
+        int next = prev + curr;
+        printf("%d\n", next);
+        prev = curr;
+        curr = next;
+    }
+}
+
 int main() {
-  printf("Hello world\n");
-  return 0;
+    int n;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    fibonacci(n);
+    return 0;
 }
